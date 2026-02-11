4d05507292bec5caba8da64ba6232c94b5bab9f2
Dakyung Seo <93422277+seoda0000@users.noreply.github.com>
2026-02-11 20:19:29 +0900

    Create 17-parameter.js

---
+++ HelloWorld/views/17-parameter.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+  </head>
+  <body>
+    <h1>parameter.ejs</h1>
+    data1: <%= data1 %><br/>
+    data2: <%= data2 %><br/>
+  </body>
+</html>
